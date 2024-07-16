9b4abdc2fcee18a01727ee1b0ca8be5f284911e1
Abdulrhman Ansary <abdalrhmanmohamedansarymaky@gmail.com>
2024-07-16 18:57:00 +0300

    An example of how to using namespace

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "myLibrary.h"
+
+using namespace std;
+using namespace myLibrary;
+
+int main(int argc, char *argv[])
+{
+    test();
+
+    return 0;
+}
---
+++ namespace_myLibrary.h
@@ -0,0 +1,11 @@
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+
+namespace myLibrary
+{
+void test()
+{
+    std::cout << "The test is passed" << std::endl;
+}
+}
+#endif
